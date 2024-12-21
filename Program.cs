023eecae8f6c23bedc1a24e9f0edf7e92f2b6c01
Евгений Кучма <desferto87@gmail.com>
2024-12-21 22:02:58 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace homeWorkCycleLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countRepeat;
+
+            Console.WriteLine("Введите количество циклов");
+            countRepeat = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < countRepeat; i++) 
+            {
+                Console.WriteLine("message " + (i + 1));
+            }
+        }
+    }
+}
